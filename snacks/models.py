0e74b6faa6e2e9887fdd3de579950fa5d22574c1
Brian Lemons <brian_lemons@hotmail.com>
2021-06-23 18:49:43 -0700

    adding more

--- snacks/models.py
+++ snacks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db.models.deletion import CASCADE
+from django.urls.base import reverse
 
 # Create your models here.
 
@@ -10,3 +11,8 @@ class Snack(models.Model):
     purchaser = models.ForeignKey(get_user_model(), on_delete=CASCADE)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title[:50]
+
+    def get_absolute_url(self):
+        return reverse("snack_detail", args=[str(self.id)])
--- snacks/views.py
+++ snacks/views.py
@@ -6,3 +6,26 @@ from .models import Snack
 
 
 # Create your views here.
+
+class SnackListView(ListView):
+    model = Snack
+    template_name = "snack_list.html"
+
+class SnackDetailView(DetailView):
+    model = Snack
+    template_name = "snack_detail.html"
+
+class SnackCreateView(CreateView):
+    model = Snack
+    template_name= "snack_create.html"
+    fields = ["title field", "purchaser", "description"]
+
+class SnackUpdateView(UpdateView):
+    model = Snack
+    template_name = "snack_update.html"
+    fields = ["title field", "purchaser", "description"]
+
+class SnackDeleteView(DeleteView):
+    model = Snack
+    template_name= "snack_delete.html"
+    success_url = reverse_lazy("snack_list")
