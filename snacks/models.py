017fbd7455ae3c39d8482cd5c1b8bae41cb95d59
Brian Lemons <brian_lemons@hotmail.com>
2021-06-23 18:32:04 -0700

    adding project with model setup and registerred to admin

---
+++ snacks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+
+class Snack(models.Model):
+    title = models.CharField(max_length=256)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=CASCADE)
+    description = models.TextField()
+
---
+++ snacks/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+from django.urls import reverse_lazy
+
+from .models import Snack
+
+
+# Create your views here.
